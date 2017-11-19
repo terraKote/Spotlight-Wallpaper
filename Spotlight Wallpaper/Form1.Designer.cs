3e84f9d65253fa05bf4bc8013f89265f46566ffe
terraKote <den4ik.kushnirenko@gmail.com>
2017-11-19 19:09:39 +0200

    initial commint

---
+++ Spotlight Wallpaper/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Spotlight_Wallpaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectionPicker = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.wallpaperButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.selectedTabLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(47)))), ((int)(((byte)(69)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.selectionPicker);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.wallpaperButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(174, 491);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(174, 106);
+            this.panel3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(36, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 44);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Spotlight\r\nWallpaper";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // selectionPicker
+            // 
+            this.selectionPicker.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.selectionPicker.Location = new System.Drawing.Point(0, 200);
+            this.selectionPicker.Name = "selectionPicker";
+            this.selectionPicker.Size = new System.Drawing.Size(5, 38);
+            this.selectionPicker.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(47)))), ((int)(((byte)(69)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(0, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "About";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.SelectWindow);
+            // 
+            // wallpaperButton
+            // 
+            this.wallpaperButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(47)))), ((int)(((byte)(69)))));
+            this.wallpaperButton.FlatAppearance.BorderSize = 0;
+            this.wallpaperButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.wallpaperButton.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wallpaperButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.wallpaperButton.Location = new System.Drawing.Point(0, 112);
+            this.wallpaperButton.Name = "wallpaperButton";
+            this.wallpaperButton.Size = new System.Drawing.Size(177, 38);
+            this.wallpaperButton.TabIndex = 2;
+            this.wallpaperButton.Text = "Wallpapers";
+            this.wallpaperButton.UseVisualStyleBackColor = false;
+            this.wallpaperButton.Click += new System.EventHandler(this.SelectWindow);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.SystemColors.Control;
+            this.closeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.Location = new System.Drawing.Point(559, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(30, 30);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "X";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.AppExit);
+            // 
+            // selectedTabLabel
+            // 
+            this.selectedTabLabel.AutoSize = true;
+            this.selectedTabLabel.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.selectedTabLabel.Location = new System.Drawing.Point(6, 30);
+            this.selectedTabLabel.Name = "selectedTabLabel";
+            this.selectedTabLabel.Size = new System.Drawing.Size(160, 33);
+            this.selectedTabLabel.TabIndex = 3;
+            this.selectedTabLabel.Text = "Wallpapers";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.selectedTabLabel);
+            this.panel2.Controls.Add(this.closeButton);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(174, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(592, 106);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BeginDragWindow);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DragWindow);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.saveButton);
+            this.panel4.Controls.Add(this.listBox1);
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Location = new System.Drawing.Point(180, 112);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(574, 367);
+            this.panel4.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(203, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 310);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(194, 355);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.SelectImage);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.saveButton.FlatAppearance.BorderSize = 0;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.Location = new System.Drawing.Point(315, 319);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(145, 39);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save As...";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.SaveImage);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "image";
+            this.saveFileDialog1.Filter = "JPEG|*.jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(766, 491);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button wallpaperButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel selectionPicker;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label selectedTabLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Spotlight Wallpaper/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spotlight_Wallpaper
+{
+    public partial class Form1 : Form
+    {
+        private Point mouseLocation;
+        private Image[] images;
+        private string[] imageResolutions;
+        private Image selectedImage;
+
+        private string pathToImages = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            selectionPicker.Top = wallpaperButton.Top;
+        }
+
+        private void AppExit(object sender, EventArgs e)
+        {
+            images = null;
+            Application.Exit();
+        }
+
+        private void SelectWindow(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch (button.Name)
+            {
+                case "wallpaperButton":
+                    images = GetImages(pathToImages, out imageResolutions);
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(imageResolutions);
+                    break;
+            }
+
+            selectionPicker.Top = button.Top;
+            selectedTabLabel.Text = button.Text;
+        }
+
+        private void DragWindow(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePos = MousePosition;
+                mousePos.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void BeginDragWindow(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private Image[] GetImages(string path, out string[] imageNames)
+        {
+            List<Image> images = new List<Image>();
+            List<string> imageSize = new List<string>();
+
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+
+                foreach (string fileName in files)
+                {
+                    try
+                    {
+                        Image image = Image.FromFile(fileName);
+
+                        if (image != null)
+                        {
+                            if (image.Width >= 300)
+                            {
+                                images.Add(image);
+
+                                // Add image resolution
+                                imageSize.Add(string.Format("{0} x {1}", image.Width, image.Height));
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+
+            imageNames = imageSize.ToArray();
+            return images.ToArray();
+        }
+
+        private void SelectImage(object sender, EventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            selectedImage = images[listBox.SelectedIndex];
+            pictureBox1.Image = selectedImage;
+        }
+
+        private void SaveImage(object sender, EventArgs e)
+        {
+            if (selectedImage == null)
+            {
+                return;
+            }
+
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                selectedImage.Save(saveFileDialog1.FileName);
+            }
+        }
+    }
+}
